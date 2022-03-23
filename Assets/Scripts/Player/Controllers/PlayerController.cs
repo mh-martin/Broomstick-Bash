38829efb6ed9f858ed6ff0202be5694afb22a1cd
mh-martin <n9mama00@students.oamk.fi>
2022-03-23 11:53:16 +0200

    flying and collision

--- Assets/Scripts/Player/Controllers/PlayerController.cs
+++ Assets/Scripts/Player/Controllers/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,18 +8,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Rigidbody2D m_rigidbody;
 
-    [SerializeField]
-    private float m_maxVelocity = 15.0f;
-
     private IPlayerControls m_controls;
-
-    private Vector3 m_velocity = Vector3.zero;
-
-    private Transform m_transform;
 
     private void Awake()
     {
-#if UNITY_ANDROID
+#if UNITY_ANDROID && !UNITY_EDITOR
         Application.targetFrameRate = 60;
 #endif
         m_controls = GetComponent<IPlayerControls>();
@@ -29,26 +20,17 @@ public class PlayerController : MonoBehaviour
         {
             gameObject.AddComponent<ProtoControls>();
         }
-        m_transform = transform;
     }
 
-    private void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (m_controls.IsFlying)
-        {
-            m_velocity.y += m_acceleration * Time.deltaTime;
-        }
-        else
-        {
-            m_velocity.y -= m_acceleration * Time.deltaTime;
-        }
-
-        m_velocity.y = Mathf.Clamp(m_velocity.y, -m_maxVelocity, m_maxVelocity);
-        m_transform.position += m_velocity * Time.deltaTime;//MovePosition(m_rigidbody.position + m_velocity);
     }
 
     private void FixedUpdate()
     {
-        m_rigidbody.MovePosition(m_transform.position);
+        if (m_controls.IsFlying)
+        {
+            m_rigidbody.AddForce(new Vector2(0, m_acceleration));
+        }
     }
 }
